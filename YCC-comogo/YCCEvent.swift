e6da9f3a83c3fb586433d4ee73e9180e44edf68e
James Jung <jamesjung@mwc-123218.dhcp.missouri.edu>
2018-11-15 13:23:13 -0600

    Added codable file

---
+++ YCC-comogo/YCCEvent.swift
@@ -0,0 +1,33 @@
+//
+//  YCCEvent.swift
+//  YCC-comogo
+//
+//  Created by James Jung on 11/15/18.
+//  Copyright © 2018 Youth Community Coalition. All rights reserved.
+//
+
+import Foundation
+
+struct YccEvent: Codable {
+    var title: String
+    var description: String
+    var start: Date
+    var end: Date
+    var category: String
+    var posted: Date
+    var location: String
+    var hostUser: String
+    var id: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case start
+        case end
+        case category
+        case posted
+        case location
+        case hostUser
+        case id = "_id"
+    }
+}
