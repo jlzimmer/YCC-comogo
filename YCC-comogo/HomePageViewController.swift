9231239582dd3a636c90a2dae32527d0bfe6a8f1
James Jung <jamesjung@mwc-123108.dhcp.missouri.edu>
2018-12-04 12:18:25 -0600

    removed the demo function for adding events (i.e. commented it out and removed IBAction connection)

--- YCC-comogo/HomePageViewController.swift
+++ YCC-comogo/HomePageViewController.swift
@@ -44,20 +44,20 @@ class HomePageViewController: BaseViewController{
     
     //The purpose of the function below is to provide a mechanism to save an event in core data storage.
     //This is an accepted event which should show up when we print from our "MyEventsViewController".
-    @IBAction func demoSaveAcceptedEvent(_ sender: UIButton) {
-        
-        let newEvent = YccEvent(title: "test", description: "test desc", start: Date(timeIntervalSinceNow: 0), end: Date(timeIntervalSinceNow: 0), category: "test category", posted: Date(timeIntervalSinceNow: 0), location: "test location", id: "test id")
-        
-        if let event = Event(title: newEvent.title, eventDescription: newEvent.description, eventStartDate: newEvent.start, eventEndDate: newEvent.end, category: newEvent.category, location: newEvent.location, postedDate: newEvent.posted, acceptedStatus: "accepted", id: newEvent.id) {
-            do {
-                let managedContext = event.managedObjectContext
-                try managedContext?.save()
-                print("Saved?")
-            } catch {
-                print("Error saving context")
-            }
-        }
-    }
+//    @IBAction func demoSaveAcceptedEvent(_ sender: UIButton) {
+//
+//        let newEvent = YccEvent(title: "test", description: "test desc", start: Date(timeIntervalSinceNow: 0), end: Date(timeIntervalSinceNow: 0), category: "test category", posted: Date(timeIntervalSinceNow: 0), location: "test location", id: "test id")
+//
+//        if let event = Event(title: newEvent.title, eventDescription: newEvent.description, eventStartDate: newEvent.start, eventEndDate: newEvent.end, category: newEvent.category, location: newEvent.location, postedDate: newEvent.posted, acceptedStatus: "accepted", id: newEvent.id) {
+//            do {
+//                let managedContext = event.managedObjectContext
+//                try managedContext?.save()
+//                print("Saved?")
+//            } catch {
+//                print("Error saving context")
+//            }
+//        }
+//    }
     //function to save an event in core data
     func saveEvent(newEvent: YccEvent){
         if let event = Event(title: newEvent.title, eventDescription: newEvent.description, eventStartDate: newEvent.start, eventEndDate: newEvent.end, category: newEvent.category, location: newEvent.location, postedDate: newEvent.posted, acceptedStatus: "undecided", id: newEvent.id) {
