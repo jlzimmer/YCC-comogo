1eca7a9092ba4a5bef644af5fb2c49c83e6f06c8
Sara Caponi <sbc436@mail.missouri.edu>
2018-12-08 20:05:18 -0600

    added remove from my events button

--- YCC-comogo/EventDetailViewController.swift
+++ YCC-comogo/EventDetailViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class EventDetailViewController: UIViewController {
 
-    var dummyEvents: DummyEvent?
+    @IBOutlet weak var removeBtn: UIButton!
+    var dummyEvents: Event?
     @IBOutlet weak var eventDes: UILabel!
     @IBOutlet weak var location: UILabel!
     @IBOutlet weak var endTime: UILabel!
@@ -24,17 +26,21 @@ class EventDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
          let color1 = UIColor(rgb: 0x00688F)
-        //let color2 = UIColor(rgb: 0xFFDD00)
+        let color2 = UIColor(rgb: 0xFFDD00)
        // let color3 = UIColor(rgb: 0x3286a5)
 
         eventTitle.textColor = color1
+        
+        removeBtn.backgroundColor = color2
+        removeBtn.layer.cornerRadius = 25
+        removeBtn.clipsToBounds = true
         
         picture.image=UIImage(named: "e2")
         eventTitle.text = actualEvent?.title
 //        let concatStartTime = (dummyEvents?.startTime)! + "  -"
         time.text = "ahh"
         location.text = actualEvent?.location
-        eventDes.text = "JKFHKSJNDASKdmlkamsdlkasmdlkamdlkewmdkladmla"
+       // eventDes.text = actualEvent?.description
         endTime.text = "AHHH"
         date.text = "November 28th 2018"
         // Do any additional setup after loading the view.
@@ -43,14 +49,36 @@ class EventDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func removeBtn(_ sender: Any) {
+        
+        removeEvent()
+        navigationController?.popViewController(animated: true)
     }
-    */
-
+    
+    func removeEvent(){
+        let ident = actualEvent?.id!
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", ident!)
+        do {
+            let grabbedEvent = try managedContext.fetch(fetchRequest)
+            if grabbedEvent.count != 0 {
+                let managedObject = grabbedEvent[0]
+                managedObject.setValue("declined", forKey: "acceptedStatus")
+                do {
+                    try managedContext.save()
+                } catch {
+                    print("could not save managed context")
+                }
+            }
+        } catch {
+            print("event could not be modified")
+        }
+        
+    }
 }
+
